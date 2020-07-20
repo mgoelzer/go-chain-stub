b4d8e6e2259ff714c307b0e6e51b6f7e81ff4e2e
Mike Goelzer <mike@protocol.ai>
2020-07-20 10:41:55 +0000

    Converted to http server

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"bytes"
 	"errors"
 	"encoding/hex"
+	"net/http"
+	"encoding/json"
+	"strconv"
 
 //	types "github.com/filecoin-project/lotus/chain/types"
 	"math/big"
@@ -54,38 +57,103 @@ type Message struct {
 }
 
 //
-// This is the go stub program.  It is compiled into all the CBOR marshalling and structs from Lotus.
-// However, all it does is listen on 443 and respond with well-formed unsigned messagess, which
+// This is the go stub program.  It is compiled into all the CBOR 
+// marshalling and structs from Lotus.  However, all it does is listen 
+// on 80 and respond with well-formed unsigned messagess, which
 // can be signed in rust/js.
 //
 
 func main() {
-	// A dummy main() to illustrate the usage
-	toBase32Str := "t1ymsm3e4tavw4dsc6raxk6dioxi4coio4sp37lzq"
-	fromBase32Str := "t1ymsm3e4tavw4dsc6raxk6dioxi4coio4sp37lzq"
-	amountBase10 := 30
-	msg, err := generateMessageForSigning(toBase32Str, fromBase32Str, amountBase10)
+	http.HandleFunc("/paymentchannel/create", PaymentChannelCreate)
+	http.ListenAndServe(":8080", nil)
+}
+
+//
+//
+// HTTP helpers
+//
+//
+func failResponse(w http.ResponseWriter, responseStatus int, message string) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(responseStatus)
+	errMsgMap := map[string]string{"Error": message}
+	jsonBodyStr,_ := json.Marshal(errMsgMap)
+	fmt.Fprintf(w, "%s", jsonBodyStr)
+}
+
+//
+// Dispatcher for all `/paymentchannel/create` actions
+//
+func PaymentChannelDispatch(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err!=nil {
+		failResponse(w, http.StatusBadRequest, "Could not parse your input")
+		return
+	}
+	vals := r.Form
+	action := vals.Get("action")
+	if action=="message" {
+		paymentChannelCreate(vals, w, r)
+	} else if action=="decodereceipt") {
+		paymentChannelDecodeReceipt(vals, w, r)
+	} else {
+		failResponse(w, http.StatusBadRequest, "Unrecognized 'action'")
+		return
+	}
+}
+
+//
+// `/paymentchannel/create` endpoint
+//
+//	POST /paymentchannel/create?action=message&
+//		to=t1ymsm3e4tavw4dsc6raxk6dioxi4coio4sp37lzq&
+//		from=t1ymsm3e4tavw4dsc6raxk6dioxi4coio4sp37lzq&
+//		amount=30
+//	Response:
+//		200 OK with body {"Version":0,.......}
+//
+//	or else faile with non-2XX response code
+//
+func paymentChannelCreate(url.Values vals, w http.ResponseWriter, r *http.Request) {
+	toStr := vals.Get("to")
+	fromStr := vals.Get("from")
+	amountStr := vals.Get("amount")
+	amount, err := strconv.ParseInt(amountStr, 10, 64)
+	if err!=nil {
+		failResponse(w, http.StatusBadRequest, "'amount' not numeric")
+		return
+	}
+	if (toStr=="") || (fromStr=="") {
+		failResponse(w, http.StatusBadRequest, "'to' and 'from' invalid")
+		return
+	}
+
+	msg, err := generateMessageForSigning(toStr, fromStr, amount)
 	if err != nil {
 		fmt.Errorf("generateMessageForSigning: faled");
 	}
 
-	fmt.Printf("msg.To (string) = '%s'\n", msg.To.String())
+
 	toHexEncoded := hex.EncodeToString(msg.To.Bytes())
-	fmt.Printf("msg.To (bytes) = 0x %s\n", toHexEncoded)
-
-	fmt.Printf("msg.From (string) = '%s'\n", msg.From.String())
 	fromHexEncoded := hex.EncodeToString(msg.From.Bytes())
-	fmt.Printf("msg.From (bytes) = 0x %s\n", fromHexEncoded)
-
+	valueBytes,_ := msg.Value.Bytes()
+	valueHexEncoded := hex.EncodeToString(valueBytes)
+	gasPriceBytes,_ := msg.GasPrice.Bytes()
+	gasPriceHexEncoded := hex.EncodeToString(gasPriceBytes)
 	paramsHexEncoded := hex.EncodeToString(msg.Params)
-	fmt.Printf("msg.Params (bytes) = 0x %s\n", paramsHexEncoded)
+	response := map[string]string{	"Version": strconv.FormatInt(msg.Version,10),
+		"To":toHexEncoded, "From": fromHexEncoded,
+		"Nonce": strconv.FormatUint(msg.Nonce,10), "Value":valueHexEncoded,
+		"GasPrice":gasPriceHexEncoded, "GasLimit":strconv.FormatInt(msg.GasLimit,10),
+		"Method":msg.Method.String() , "Params":paramsHexEncoded}
+	responseJsonStr,_ := json.Marshal(response)
 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", responseJsonStr)
 }
 
-//
-// `/paymentchannel/create/` endpoint
-//
-func generateMessageForSigning(toBase32Str string, fromBase32Str string, amountBase10 int) (*Message, error) {
+func generateMessageForSigning(toBase32Str string, fromBase32Str string, amountBase10 int64) (*Message, error) {
 	// From and To addresses  are just whatever user typed on CLI
 	from, err := address.NewFromString(toBase32Str)
 	if err != nil {
@@ -100,7 +168,7 @@ func generateMessageForSigning(toBase32Str string, fromBase32Str string, amountB
 	}
 
 	//30 is just an Int with whatever number the user enters on CLI.
-	amt := BigInt{Int: big.NewInt(30)}
+	amt := BigInt{Int: big.NewInt(amountBase10)}
 
 
 	///////////////////////////////////////////////////////////////
@@ -148,6 +216,11 @@ func generateMessageForSigning(toBase32Str string, fromBase32Str string, amountB
 //
 // `/paymentchannel/create?action=poll&mcid=<cid>&message_confidence=5`
 //
+func PaymentChannelPoll() {
+	// It may make  more sense to do this call against a chain indexer instance
+	// that has the whole history of the chain.
+}
+
 func pollForReceipt(mcid cid.Cid, MessageConfidence uint64) (int64, string, uint64, error) {
 	// The purpose of this API call is to let client poll until submitted message's Receipt
 	// can be retruned, which is MessageConfidence epochs after the message receipt is found.
@@ -166,6 +239,10 @@ func pollForReceipt(mcid cid.Cid, MessageConfidence uint64) (int64, string, uint
 //
 // `/paymentchannel/create?action=decode_receipt?exit_code=0&returnbytes=%0f%20%10%00%10...&gasUsed=10000
 //
+func paymentChannelDecodeReceipt(url.Values vals, w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
 func decodeReceipt(exitCode int64, returnBytes []byte, gasUsed uint64) (*address.Address,error) {
 	// This function decodes the receipt and returns the payment channel address.
 	var decodedReturn init_.ExecReturn
